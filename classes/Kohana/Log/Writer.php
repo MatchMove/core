246db1a43dea2d1b878c0372b476b688f0924d85
Gian Carlo Val Ebao <gianebao@gmail.com>
2013-08-20 14:59:01 +0800

    added log writer fix

--- classes/Kohana/Log/Writer.php
+++ classes/Kohana/Log/Writer.php
@@ -12,21 +12,21 @@ abstract class Kohana_Log_Writer {
 
 	/**
 	 * @var  string  timestamp format for log entries.
-	 * 
+	 *
 	 * Defaults to Date::$timestamp_format
 	 */
 	public static $timestamp;
 
 	/**
 	 * @var  string  timezone for log entries
-	 * 
+	 *
 	 * Defaults to Date::$timezone, which defaults to date_default_timezone_get()
 	 */
 	public static $timezone;
 
 	/**
 	 * Numeric log level to string lookup table.
-	 * @var array 
+	 * @var array
 	 */
 	protected $_log_levels = array(
 		LOG_EMERG   => 'EMERGENCY',
@@ -66,9 +66,9 @@ abstract class Kohana_Log_Writer {
 		return spl_object_hash($this);
 	}
 
-/**
+	/**
 	 * Formats a log entry.
-	 * 
+	 *
 	 * @param   array   $message
 	 * @param   string  $format
 	 * @return  string
@@ -76,25 +76,20 @@ abstract class Kohana_Log_Writer {
 	public function format_message(array $message, $format = "time --- level: body in file:line")
 	{
 		$message['time'] = Date::formatted_time('@'.$message['time'], Log_Writer::$timestamp, Log_Writer::$timezone, TRUE);
-        $message['level'] = $this->_log_levels[$message['level']];
+		$message['level'] = $this->_log_levels[$message['level']];
 
-        unset($message['trace']);
-        if (isset($message['additional']['exception'])) {
-            $exception = $message['additional']['exception'];
-            unset($message['additional']);
-        }
-        
-        $string = strtr($format, $message);
-        if (isset($exception))
-        {
-            // Re-use as much as possible, just resetting the body to the trace
-            $message['body'] = $exception->getTraceAsString();
-            $message['level'] = $this->_log_levels[Log_Writer::$strace_level];
+		$string = strtr($format, array_filter($message, 'is_scalar'));
 
-            $string .= PHP_EOL.strtr($format, $message);
-        }
+		if (isset($message['additional']['exception']))
+		{
+			// Re-use as much as possible, just resetting the body to the trace
+			$message['body'] = $message['additional']['exception']->getTraceAsString();
+			$message['level'] = $this->_log_levels[Log_Writer::$strace_level];
 
-        return $string;
+			$string .= PHP_EOL.strtr($format, array_filter($message, 'is_scalar'));
+		}
+
+		return $string;
 	}
 
-} // End Kohana_Log_Writer
+}
